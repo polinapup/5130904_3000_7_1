8c5f34a99b027bc144e8695207e27e91534475b5
kirillbaykin <kirkir0505@gmail.com>
2024-05-13 13:17:16 +0300

    First try for T3

---
+++ bajkin.kirill/T3/StreamGuard.h
@@ -0,0 +1,25 @@
+#ifndef STREAM_GUARD_HPP
+#define STREAM_GUARD_HPP
+#include <ios>
+
+namespace bajkin
+{
+  class StreamGuard
+  {
+  public:
+    StreamGuard(std::basic_ios< char >& s) : s_(s),
+      precision_(s.precision()),
+      flags_(s.flags())
+    {}
+    ~StreamGuard()
+    {
+      s_.precision(precision_);
+      s_.flags(flags_);
+    }
+  private:
+    std::basic_ios< char >& s_;
+    std::streamsize precision_;
+    std::basic_ios< char >::fmtflags flags_;
+  };
+}
+#endif
